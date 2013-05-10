9f98f2f9018f4fee16e30481deec1e0c1a8109eb
Dave Konopka <dkonopka@articulate.com>
2013-05-10 13:24:00 -0400

    Only one result per sentinal. Extract settings values.

--- app/models/mongodb_read_only_sentinal.rb
+++ app/models/mongodb_read_only_sentinal.rb
@@ -2,15 +2,23 @@ require 'mongo'
 include Mongo
 
 class MongodbReadOnlySentinal < Sentinal
-  def run
-    mongo     = MongoClient.new('localhost', 27017)
-    db            = mongo['sample-db']
-    collection = db['test']
+  attr_accessor :host, :port, :database, :collection, :document_count
 
-    result = Result.new
-    result.sentinal     = self
-    result.run_at       = DateTime.new
-    result.successful = collection.count > 0
+  def run
+    # Temporary until we hook up form -> database values 
+    self.host = 'localhost'
+    self.port = '27017'
+    self.database = 'sample-db'
+    self.collection = 'test'
+    self.document_count = 2
+
+    mongo = MongoClient.new(self.host, self.port)
+    db = mongo[self.database]
+    collection = db[self.collection]
+
+    result = Result.where(sentinal_id: self.id).first_or_create
+    result.run_at = Time.now
+    result.successful = (collection.count == self.document_count)
     result.save
   end
 end
