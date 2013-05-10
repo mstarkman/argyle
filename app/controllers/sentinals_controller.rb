4270163cbed17915b7b53d8c1a8f2e7cf3f6367d
Mark Starkman <mstarkman@articulate.com>
2013-05-10 10:27:32 -0400

    started the ability to create a sentinal

--- app/controllers/sentinals_controller.rb
+++ app/controllers/sentinals_controller.rb
@@ -1,4 +1,12 @@
 class SentinalsController < ApplicationController
+  respond_to :html, only: :index
+  respond_to :js, only: [:new, :create, :destroy]
+  
   def index
+    @sentinals = Sentinal.all
+  end
+
+  def new
+    @sentinal = Sentinal.new
   end
 end
--- app/models/sentinal.rb
+++ app/models/sentinal.rb
@@ -1,2 +1,3 @@
 class Sentinal < ActiveRecord::Base
+  validates_presence_of :name
 end
